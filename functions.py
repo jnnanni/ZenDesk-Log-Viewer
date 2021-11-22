40ced98bd34fb2456cd04281988e467abbd3e80e
jnnanni <jnnanni@umich.edu>
2021-11-21 21:25:10 -0500

    Added Full user input loop and error checking for both inputs and API requests

---
+++ functions.py
@@ -0,0 +1,59 @@
+def clearConsole():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def printTickets(table):
+    columnList = ["id","created_at","updated_at","subject","status"]
+    df = pd.DataFrame(table)
+    df["created_at"]=df["created_at"].apply(lambda x: convertFromZulu(x))
+    df["updated_at"]=df["updated_at"].apply(lambda x: convertFromZulu(x))
+    print(tabulate(df.loc[:,columnList],headers=columnList,showindex=False))
+
+def convertFromZulu(time):
+    return str(datetime.strptime(time,"%Y-%m-%dT%H:%M:%SZ")) + " UTC"
+
+def printIndividualTicket(table,id):
+    startID = int(table[0]["id"])
+    while(id - startID not in range(0,25)):
+        print("Valid ID Numbers for this page include "+str(startID)+"-"+str(startID+len(table)-1))
+        id = int(input("Inavlid ID entered, please reenter: "))
+    index = id - startID
+    entry = table[index]
+    print("Subject: " + entry["subject"])
+    print("Status: "+ entry["status"])
+    print("ID: "+ str(entry["id"]))
+    print('-'*100)
+    print("Description:")
+    print(entry["description"])
+    print('-'*100)
+    print("Created at: "+ convertFromZulu(entry["created_at"]))
+    print("Updated at: "+ convertFromZulu(entry["updated_at"]))
+    print("Submitter ID: " + str(entry["submitter_id"]))
+    return True
+    
+
+
+def promptUser(prev=False,next=False):
+    validInput = ["1","2","q"]
+    print("[1] to reprint all entries on the current page")
+    print("[2] to view an individual ticket on this page")
+    if(prev):
+        print("[3] to go to previous page")
+        validInput.append("3")
+    if(next):
+        print("[4] to go to next page")
+        validInput.append("4")
+    print("[q] to exit")
+
+    command = ""
+    while command not in validInput:
+        command = input()
+        if(command not in validInput):
+            print("Invalid Command, please reenter command:")
+    return command
+
+def checkCursor(address,username,password):
+    if(address is None):
+        return False
+    r = requests.get(address, auth=(username,password), timeout=2)
+    ticketInfo = r.json()
+    return bool(ticketInfo["tickets"])
--- main.py
+++ main.py
@@ -1,74 +1,87 @@
+from numpy import add
 import requests
 import os
 import sys
 from getpass import getpass
-import json
 import pandas as pd
+from requests.exceptions import TooManyRedirects
+from requests.models import HTTPError
 from tabulate import tabulate
 from datetime import datetime
-
-def clearConsole():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-def printPage(link,username,password):
-    clearConsole()
-    print("Fetching ticket data...")
-    r = requests.get(link, auth=(username,password), timeout=2)
-    clearConsole()
-    ticketInfo = r.json()
-    #printTableInfo(ticketInfo["tickets"])
-    printIndividualTicket(ticketInfo["tickets"],35)
-
-def printTableInfo(table):
-    columnList = ["id","type","created_at","updated_at","subject","status"]
-    df = pd.DataFrame(table)
-    print(tabulate(df.loc[:,columnList],headers=columnList,showindex=False))
-
-def convertFromZulu(time):
-    return str(datetime.strptime(time,"%Y-%m-%dT%H:%M:%SZ")) + " UTC"
-
-def printIndividualTicket(table,id):
-    startID = table[0]["id"]
-    print(startID)
-    entry = table[id - startID]
-    print("Subject: " + entry["subject"])
-    print("Status: "+ entry["status"])
-    print('-'*100)
-    print("Description:")
-    print(entry["description"])
-    print('-'*100)
-    print("Created at: "+ convertFromZulu(entry["created_at"]))
-    print("Updated at: "+ convertFromZulu(entry["updated_at"]))
-    print("Submitter ID: " + str(entry["submitter_id"]))
-
+from functions import *
 
 username = input("Enter your Email Address: ")
 #password = getpass(prompt="Enter your password: ")
 password = input("Enter your password: ")
+address = "https://zccjnnanni.zendesk.com/api/v2/tickets.json?page[size]=25"
 
 clearConsole()
-
 print("Fetching ticket data...")
-r = requests.get("https://zccjnnanni.zendesk.com/api/v2/tickets.json", auth=(username,password), timeout=2)
+r = requests.get(address, auth=(username,password), timeout=2)
 clearConsole()
 
 ticketInfo = r.json()
 
-if 'error' in ticketInfo.keys():
+if "error" in ticketInfo.keys(): #Error Checking for Username/Password
     print("ERROR: could not authenticate your username and/or password")
     print(" [1] to retry")
     print(" Any other key to exit")
     error = input()
-    if(error == '1'):
-        os.execv(sys.executable, ['python'] + sys.argv)
+    if(error == "1"):
+        os.execv(sys.executable, ["python"] + sys.argv)
     else:
         exit(1)
 
-
 print("Successfully fetched Ticket Data")
-page = 0
 
-file = open('test.json', 'w')
-json.dump(ticketInfo,file,indent=6)
-file.close()
+page = 1
+command = ""
+firstPageView = True
+while(command!="q"):
+    try:
+        prev = checkCursor(ticketInfo["links"]["prev"],username,password)
+        next = checkCursor(ticketInfo["links"]["next"],username,password)
+        if firstPageView:
+            clearConsole()
+            printTickets(ticketInfo["tickets"])
+            firstPageView = False
+        print("Current Page: " + str(page))
+        command = promptUser(prev,next)
+        if(command == "1"):
+            clearConsole()
+            printTickets(ticketInfo["tickets"])
+            continue
+        if(command == "2"):
+            command = int(input("Choose an ID to view: "))
+            clearConsole()
+            printIndividualTicket(ticketInfo["tickets"],command)
+            continue
+        if(command == "3"):
+            clearConsole()
+            firstPageView = True
+            r = requests.get(ticketInfo["links"]["prev"], auth=(username,password), timeout=2)
+            ticketInfo = r.json()
+            page = page - 1
+            continue
+        if(command == "4"):
+            clearConsole()
+            firstPageView = True
+            r = requests.get(ticketInfo["links"]["next"], auth=(username,password), timeout=2)
+            ticketInfo = r.json()
+            page = page + 1
+            continue
+    except(TimeoutError,ConnectionError,HTTPError,TooManyRedirects): #Error Checking for input loop
+        print("Error Encountered in API Request")
+        print("[1] to retry")
+        print("[q] to exit")
+        command = input()
+        while(command not in ["1","q"]):
+            print("[1] to retry")
+            print("[q] to exit")
+            command = input()
+        if(command == "1"):
+            clearConsole()
+            continue
+        if(command == "q"):
+            break
 

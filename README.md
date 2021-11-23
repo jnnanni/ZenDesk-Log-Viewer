5ade0c83d41956748fdb039b24d7ff8e00aec67b
jnnanni <jnnanni@umich.edu>
2021-11-22 19:16:06 -0500

    Added domain.cfg so that subdomain can be changed and input redirection can be done without changing the code. Also added error handling to account for invalid values in the domain.cfg file

--- README.md
+++ README.md
@@ -1,17 +1,26 @@
-Installation:
+***Installation:***
 1. Make sure that you have installed Python 3 on your machine
-2. In a terminal at the directory of the program, run the following command:<br />
-pip install -r requirements.txt<br />
+2. In a terminal at the directory of the program, run the following command:<br/>
+pip install -r requirements.txt<br/>
 This will ensure all the proper python packages are installed in order for you
-to use the ticket viewer<br />
+to use the ticket viewer<br/>
 
-Usage:<br />
-Start the program by using "python3 main.py" in the working directory of the program<br />
-The program will ask for your username and password for my zendesk domain<br />
-(Done so that I would not have to commit my Username and password to GitHub)<br />
-The program will then fetch the first 25 entries from the API and prompt you to input one of the following:<br />
-    [1] Reprint the entries on the current page<br />
-    [2] View more details on a specific log entry that is part of the current page<br />
-    [3] (If a previous page exists) navigate to the previous page of tickets<br />
-    [4] (If a next page exists) navigate to the next page of tickets<br />
+
+***Config:***<br/>
+**Domain:**<br/>
+The zendesk domain that you will use to access the tickets from<br/>
+**Password Input Redirection:**<br/>
+Set to 0 to hide password input, since the hidden password input will not work with input redirection, <br/>
+setting to 1 will allow for input redirection such as "python3 main.py < commands.txt"<br/>
+
+
+***Usage:***<br/>
+Start the program by using "python3 main.py" in the working directory of the program<br/>
+The program will ask for your username and password for my zendesk domain<br/>
+(Done so that I would not have to commit my Username and password to GitHub)<br/>
+The program will then fetch the first 25 entries from the API and prompt you to input one of the following:<br/>
+    [1] Reprint the entries on the current page<br/>
+    [2] View more details on a specific log entry that is part of the current page<br/>
+    [3] (If a previous page exists) navigate to the previous page of tickets<br/>
+    [4] (If a next page exists) navigate to the next page of tickets<br/>
     [q] Exit the program
--- main.py
+++ main.py
@@ -4,11 +4,19 @@ from requests.exceptions import TooManyRedirects
 from requests.models import HTTPError
 from functions import *
 
+f = open("domain.cfg","r")
+config = f.readlines()
+config = [val.rstrip() for val in config]
+address = config[1]+"/api/v2/tickets.json?page[size]=25"
+if(config[3] not in ["0","1"]):
+    print("Error in domain.cfg: \'Input Redirection for Password\' must be set to either 0 or 1")
+    exit(1)
 username = input("Enter your Email Address: ")
-password = getpass(prompt="Enter your password: ")
-#Uncomment 10 and comment 8 for use with input redirection
-#password = input("Enter your password: ")
-address = "https://zccjnnanni.zendesk.com/api/v2/tickets.json?page[size]=25"
+if(bool(int(config[3]))):
+    password = input("Enter your password: ")
+else:
+    password = getpass(prompt="Enter your password: ")
+
 
 while(True):
     try:
@@ -17,8 +25,9 @@ while(True):
         r = requests.get(address, auth=(username,password), timeout=2)
         clearConsole()
         break
-    except(TimeoutError,ConnectionError,HTTPError,TooManyRedirects): #Error Checking for initial read
-            print("Error Encountered in API Request")
+    except(TimeoutError,ConnectionError,HTTPError,TooManyRedirects) as e: #Error Checking for initial read
+            print("Error Encountered in Initial API Request:")
+            print(e)
             print("[1] to retry")
             print("[q] to exit")
             command = input()
@@ -32,15 +41,22 @@ while(True):
                 exit(1)
 
 ticketInfo = r.json()
-
-if "error" in ticketInfo.keys(): #Error Checking for Username/Password
-    print("ERROR: could not authenticate your username and/or password")
-    print(" [1] to retry")
-    print(" Any other key to exit")
-    error = input()
-    if(error == "1"):
-        os.execv(sys.executable, ["python"] + sys.argv)
+if "error" in ticketInfo.keys(): #Error Checking for Username/Password and Subdomain
+    if(type(ticketInfo["error"])==type({})):
+        if("No help desk at" in ticketInfo["error"]["title"]):
+            print("ERROR: Invalid Domain found in domain.cfg, please check the file before retrying")
     else:
+        print("ERROR: could not authenticate your username and/or password")
+    print("[1] to retry")
+    print("[q] to exit")
+    command = input()
+    while(command not in ["1","q"]):
+        print("[1] to retry")
+        print("[q] to exit")
+        command = input()
+    if(command == "1"):
+        os.execv(sys.executable, ["python"] + sys.argv)
+    elif(command == "q"):
         exit(1)
 
 print("Successfully fetched Ticket Data")
@@ -82,8 +98,9 @@ while(command!="q"):
             ticketInfo = r.json()
             page = page + 1
             continue
-    except(TimeoutError,ConnectionError,HTTPError,TooManyRedirects,KeyError): #Error Checking for input loop
+    except(TimeoutError,ConnectionError,HTTPError,TooManyRedirects,KeyError) as e: #Error Checking for input loop
         print("Error Encountered in API Request")
+        print(e)
         print("[1] to retry")
         print("[q] to exit")
         command = input()
